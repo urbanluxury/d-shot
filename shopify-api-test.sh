0ae7412034b760746fc25746392bacabe7faf231
Antonio Martin <support@ulxstudios.com>
2026-01-24 05:41:59 -0800

    Remove temporary API test scripts

--- shopify-api-test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Test Shopify Admin API
-
-ADMIN_TOKEN="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"
-STORE="d-shot-2.myshopify.com"
-
-curl -s -X GET "https://${STORE}/admin/api/2024-01/products.json?limit=5" \
-  -H "X-Shopify-Access-Token: ${ADMIN_TOKEN}" \
-  -H "Content-Type: application/json"
